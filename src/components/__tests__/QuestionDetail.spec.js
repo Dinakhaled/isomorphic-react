cb243f8f5861cbc4580e57869d8bca06850ba6f8
Dinakhaled <dina.elghndour@gmail.com>
2020-05-09 16:12:01 +0200

    Async test methods

--- src/components/__tests__/QuestionDetail.spec.js
+++ src/components/__tests__/QuestionDetail.spec.js
@@ -10,8 +10,4 @@ describe('The Question Detail component', () => {
   it.only(`It should not regress`, () => {
     expect(2 + 2).toEqual(4);
   })
-
-  it(`It should not regress`, () => {
-    expect(40 + 2).toEqual(43);
-  })
 })
